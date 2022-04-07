43aa318fe74e42917b523c4ef4f514f67062e4dc
Omience <patfun10@gmail.com>
2022-04-07 14:57:49 -0700

    !

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
 #include <stdio.h>                                                                                                                      
+
 /**                                                                                                                                     
-   * main - Entry point                                                                                                                                                                                                                                                        
+ 
+   * main - Entry point                                                                                                                   
+ 
+    *                                                                                                                                      
+ 
      * Return: 0 printing the function                                                                                                      
+ 
       */                                                                                                                                     
+
 int main(void)                                                                                                                          
-{                                                                                                                                       
+
+{                                                                                                                                     
 	puts("\"Programming is like building a multilingual puzzle");                                                                           
-	return (0);                                                                                                                             
-}                                                    
+	return (0);
+}	
