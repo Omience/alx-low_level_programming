470c34ad6a6dbe84cecadccbaf0b57562b5c40ea
Omience <patfun10@gmail.com>
2022-05-09 14:31:56 -0700

    Done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
-  *malloc_checked - allocate memory.
-  *@b: size of the memory to be allocated.
-  *Return: pointer;
-  **/
+ *malloc_checked - allocate memory.
+ *@b: size of the memory to be allocated.
+ *Return: pointer;
+ **/
 
 void *malloc_checked(unsigned int b)
 {
@@ -15,7 +15,7 @@ void *malloc_checked(unsigned int b)
 	if (p == NULL)
 	{
 		exit(98);
-		return (NULL)
+		return (NULL);
 	}
 	return (p);
 }
