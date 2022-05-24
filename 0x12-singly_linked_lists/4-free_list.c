bd05e8eea1b657f6983b3d906d4ec3b19e28679d
Omience <patfun10@gmail.com>
2022-05-24 13:15:22 -0700

    0

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 
 /**
- * free_list - free linked list
- * @head: linked list
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
  */
 
 void free_list(list_t *head)
 {
-	while (head != NULL)
+	list_t *current;
+
+	while ((current = head) != NULL)
 	{
-		free(head->str);
-		free(head);
 		head = head->next;
+		free(current->str);
+		free(current);
 	}
 }
-© 2022 GitHub, Inc.
-Terms
